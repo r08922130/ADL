ac7a57c78926d6ad337464632eeb1d8c075e1350
r08922130 <cp4n3gj9448@gmail.com>
2020-03-27 10:00:31 +0800

    plot

--- HW1/solver.py
+++ HW1/solver.py
@@ -4,21 +4,31 @@ import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 from postprocessing import Postprocessing
+import matplotlib.pyplot as plt
 class Solver:
     def __init__(self):
         super().__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    def plot(self,x,y,x_val,y_val):
+        plt.figure()
+        plt.plot(x,y,"r",x_val,y_val,"b")
+        plt.show()
     def train(self,seq_model,batches,labels,mul,valid_batches,valid_labels,val_mul,device,mode='extractive',
                 criterion=nn.BCEWithLogitsLoss(),epoch=10,lr=0.01,encoder=None,decoder=None):
         
         min_loss = 100000000
         best_model = None
         seq_opt=optim.RMSprop(seq_model.parameters(), lr=lr)
+        step = 0
+        x_train = []
+        loss_train =[]
+        x_val = []
+        loss_val = []
         if mode == 'extractive':
             for ep in range(epoch):
                 seq_model.train()
                 bl = len(batches)
+                total_loss=0
                 for i in range(bl):
                     seq_opt.zero_grad()
                     data = torch.LongTensor(batches[i]).to(device)
@@ -41,11 +51,12 @@ class Solver:
                     loss.backward()
                     torch.nn.utils.clip_grad_norm_(seq_model.parameters(),10)
                     seq_opt.step()
-                    
-
+                    step+=1
+                    total_loss+= loss.item()
                     if i % 100 == 0:
                         #print(data)
-                        
+                        x_train+= [step]
+                        loss_train += [total_loss/step]
                             #print(pred.permute(1,0)[0])
                         print("Train epoch : {}, step : {} / {}, loss : {}".format(ep, i,bl,loss.item()))
                 # validation
@@ -64,13 +75,18 @@ class Solver:
                     loss = criterion(pred.view(pred.size()[0],pred.size()[1])*m, target) 
 
                     total_loss += loss.item()
+                    step+=1
+                    
                     if i % 100 == 0:
+                        x_val+= [step]
+                        loss_val += [total_loss/step]
                         print("Valid epoch : {}, step : {} / {}, loss : {}".format(ep, i,bl,loss.item()))
                 if min_loss > total_loss:
                     min_loss =total_loss
                     best_model = seq_model
                 if ep %10 == 0:
                     torch.save(best_model.state_dict(), "ckpt/best.ckpt")
+            self.plot(x_train,loss_train,x_val,loss_val)
             seq_model = best_model
                     
     def train_sentences(self,seq_model,batches,labels,valid_batches,valid_labels,device,mode='extractive',
@@ -79,6 +95,11 @@ class Solver:
         min_loss = 100000000
         best_model = None
         seq_opt=optim.RMSprop(seq_model.parameters(), lr=lr)
+        step = 0
+        x_train = []
+        loss_train =[]
+        x_val = []
+        loss_val = []
         if mode == 'extractive':
             for ep in range(epoch):
                 seq_model.train()
@@ -105,10 +126,12 @@ class Solver:
                     seq_opt.step()
                     
 
+                    step+=1
+                    total_loss+= loss.item()
                     if i % 100 == 0:
                         #print(data)
-                        #print(pred.permute(1,0))
-                            #print(pred.permute(1,0)[0])
+                        x_train+= [step]
+                        loss_train += [total_loss/step]
                         print("Train epoch : {}, step : {} / {}, loss : {}".format(ep, i,bl,loss.item()))
                 # validation
                 seq_model.eval()
@@ -126,13 +149,17 @@ class Solver:
                     loss = criterion(pred.view(pred.size()[0],pred.size()[1]), target) 
 
                     total_loss += loss.item()
+                    step +=1
                     if i % 100 == 0:
+                        x_val+= [step]
+                        loss_val += [total_loss/step]
                         print("Valid epoch : {}, step : {} / {}, loss : {}".format(ep, i,bl,loss.item()))
                 if min_loss > total_loss:
                     min_loss =total_loss
                     best_model = seq_model
                 if ep %10 == 0:
                     torch.save(best_model.state_dict(), "ckpt/best.ckpt")
+            self.plot(x_train,loss_train,x_val,loss_val)
             seq_model = best_model
     def test(self,seq_model,batches,interval,output_file,device,mode='test'):
         result = []
